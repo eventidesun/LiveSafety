facd939874bf587f5b7bdf7f943b6b864054bf61
eventidesun <vgudimel@uwo.ca>
2024-01-13 22:36:58 -0500

    home page

--- LiveSafety/LiveSafety/HomePage.swift
+++ LiveSafety/LiveSafety/HomePage.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct HomePage: View {
+    @State private var showAlert = false
+    
     var body: some View {
         ZStack {
             // Background
@@ -16,6 +18,7 @@ struct HomePage: View {
             // Main content
             VStack(alignment: .center) {
                 greetingText
+                Spacer()
                 sendAlertButton
                 undoActionButton
             }
@@ -26,23 +29,33 @@ struct HomePage: View {
     private var greetingText: some View {
         HStack {
             Text("Hello!")
-                .font(.title)
+                .font(.largeTitle)
                 .fontWeight(.semibold)
                 .foregroundColor(.white)
                 .padding(.leading)
             Spacer() // Pushes the text to the top
         }
-        .padding(.top) // Add padding to the top edge
+        .padding(EdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 0))
+
     }
 
     private var sendAlertButton: some View {
-        Text("Send Alert")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .foregroundColor(.white)
-            .frame(width: 300, height: 100)
-            .background(Color(red: 82/255, green: 0/255, blue: 255/255))
-            .cornerRadius(30)
+        Button(action: {
+            self.showAlert = true
+        }) {
+            Text("Send Alert")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .frame(width: 300, height: 100)
+                .background(Color(red: 82/255, green: 0/255, blue: 255/255))
+                .cornerRadius(30)
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Alert Sent"), message: Text("Your alert and live location have been sent successfully."))
+        }
+        
+        
     }
 
     private var undoActionButton: some View {
@@ -57,7 +70,8 @@ struct HomePage: View {
                 .foregroundColor(Color(red: 82/255, green: 0/255, blue: 255/255))
                 .padding(.top, -5)
         }
-        .padding()
+        .padding([.top, .bottom], 10)
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 300, trailing: 0))
     }
     
     
