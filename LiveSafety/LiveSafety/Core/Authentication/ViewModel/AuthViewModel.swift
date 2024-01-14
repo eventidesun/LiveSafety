3220f4e506ffe7d60b94de018b5b188f8ada1831
anya <124650041+anyaliuu@users.noreply.github.com>
2024-01-13 22:31:16 -0500

    wer

---
+++ LiveSafety/LiveSafety/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewModel.swift
+//  LiveSafety
+//
+//  Created by Anne Liu on 2024-01-13.
+//
+
+import SwiftUI
+
+struct AuthViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthViewModel()
+}
