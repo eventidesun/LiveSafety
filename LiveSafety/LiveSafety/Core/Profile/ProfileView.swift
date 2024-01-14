ae9d4e2688b311c58fdc1bfb610351e71bb379a1
anya <124650041+anyaliuu@users.noreply.github.com>
2024-01-13 22:08:40 -0500

    sdfs

---
+++ LiveSafety/LiveSafety/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  LiveSafety
+//
+//  Created by Anne Liu on 2024-01-13.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
