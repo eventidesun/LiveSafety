4793336d323efa8b6391ccdd3558a850c1668212
eventidesun <vgudimel@uwo.ca>
2024-01-13 21:45:18 -0500

    updates

--- LiveSafety/LiveSafety/HomePage.swift
+++ LiveSafety/LiveSafety/HomePage.swift
@@ -9,40 +9,48 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        
-        ZStack{
-            Color(.black)
-                .ignoresSafeArea()
-            
-            VStack(alignment: .center){
-                Text("Hi!")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .frame(width: 200, height: 200, alignment: .top)
-                
-                Text("Send Alert")
-                    .font(.largeTitle)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(20)
-                    .background(Color.red)
-                    .frame(width: 300, height: 100, alignment: .center)
-                
-                Text("Undo Action")
-                    .font(.title3)
-                    .foregroundColor(Color.white)
-               
-                }
-           
+        ZStack {
+            // Background
+            Color.black.ignoresSafeArea()
+
+            // Main content
+            VStack(alignment: .center) {
+                greetingText
+                sendAlertButton
+                undoActionButton
+            }
         }
-        
-        
-        }
-       
     }
 
+    // Extracted subviews for better readability and reusability
+    private var greetingText: some View {
+        Text("Hi!")
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+            .frame(width: 200, height: 200, alignment: .top)
+    }
+
+    private var sendAlertButton: some View {
+        Text("Send Alert")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(width: 300, height: 100)
+            .background(Color(red: 82/255, green: 0/255, blue: 255/255))
+            .cornerRadius(30)
+    }
+
+    private var undoActionButton: some View {
+        Text("Undo Action")
+            .font(.title3)
+            .foregroundColor(.white)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .underline()
+            .padding()
+    }
+}
 
 #Preview {
     HomePage()
