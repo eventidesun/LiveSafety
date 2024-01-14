f2cc31c77c0b1a62b18ff3b2610deaca30e922db
Vaishnavi Gudimella <118631877+eventidesun@users.noreply.github.com>
2024-01-13 21:00:40 -0500

    Add files via upload

---
+++ LiveSafety/LiveSafety/LiveSafetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSafetyApp.swift
+//  LiveSafety
+//
+//  Created by Vaishnavi Gudimella on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct LiveSafetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
